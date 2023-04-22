3d6a741de2385c12349b066af31e7ad1badf2e77
Vatsal <rajvatsal2004@gmail.com>
2023-04-22 15:15:19 +0530

    MAJOR:Create a function game()
    This function will play a game of 5 rounds and show the score
    at the end.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice(){
 function getPlayerChoiceNumber(choicePlayer){
     let choicePlayerNumber = 0;
 
-    //converts the choice from string to number for calculations
+    //converts the choice from string to number for calculations on showRoundWinner()
     if (choicePlayer = "ROCK"){
         choicePlayerNumber = 1;
     }else if(choicePlayer = "PAPER"){
@@ -58,12 +58,44 @@ function showRoundWinner(choiceComputer, choicePlayer){
         }else if (choiceComputer === 2){
             // Scissors vs Paper
             roundResult = "I chose PAPER. You WON!!";
-        }else{
+        }else if(playerChoice = "SCISSORS"){
             // Scissors vs Scissors
             roundResult = "Draw";
+        }else{
+            console.log("Invalid String. Be sure to not leave any space or make any typos")
         }
     }
 
     return roundResult;
     
+}
+
+function game(){
+    let scorePlayer = 0, scoreComputer = 0, choicePlayer = "", originalStringPlayerChoice = "", roundResult = "";
+
+    for(i = 1; i < 6 ; i++){
+        choicePlayer = prompt("Enter your move Rock, Paper or Scissors. NO TYPOS or it won't work!!! ")
+
+        //to store original string to later console.log player's choice without any change made to it
+        originalStringPlayerChoice = choicePlayer;
+
+        choicePlayer = choicePlayer.toUpperCase();
+        roundResult = showRoundWinner(getComputerChoice(), getPlayerChoiceNumber(choicePlayer));
+        
+        if (roundResult.search("You WON") === -1 && roundResult.search("Draw") === -1){
+            ++scoreComputer;
+        }else if(roundResult.search("Draw") === -1){
+            ++scorePlayer;
+        }
+        
+        console.log("\n------------------ROUND " + i + "------------------");
+        console.log("\nYOUR MOVE IS " + originalStringPlayerChoice);
+        console.log("\n" + roundResult);
+        
+        if (i === 5){
+            console.log("*********************END**********************");
+            console.log("\nComputer Score is " + scoreComputer + "\n Player Score is " + scorePlayer);            
+        }
+
+    }
 }
